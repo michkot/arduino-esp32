d2be54598a68115bff697ce40b9a3d8e2f540dcb
michkot <michkot@users.noreply.github.com>
2022-12-02 04:02:15 +0100

    WiFiServer: end() releases pre-accepted connections; simplify code

--- libraries/WiFi/src/WiFiServer.cpp
+++ libraries/WiFi/src/WiFiServer.cpp
@@ -39,23 +39,12 @@ size_t WiFiServer::write(const uint8_t *data, size_t len){
 void WiFiServer::stopAll(){}
 
 WiFiClient WiFiServer::available(){
-  if(!_listening)
-    return WiFiClient();
-  int client_sock;
-  if (_accepted_sockfd >= 0) {
-    client_sock = _accepted_sockfd;
+  if (! _accepted_sockfd >= 0) {
+    hasClient();
+  }
+  if(_accepted_sockfd >= 0){
+    int client_sock = _accepted_sockfd;
     _accepted_sockfd = -1;
-  }
-  else {
-  struct sockaddr_in _client;
-  int cs = sizeof(struct sockaddr_in);
-#ifdef ESP_IDF_VERSION_MAJOR
-    client_sock = lwip_accept(sockfd, (struct sockaddr *)&_client, (socklen_t*)&cs);
-#else
-    client_sock = lwip_accept_r(sockfd, (struct sockaddr *)&_client, (socklen_t*)&cs);
-#endif
-  }
-  if(client_sock >= 0){
     int val = 1;
     if(setsockopt(client_sock, SOL_SOCKET, SO_KEEPALIVE, (char*)&val, sizeof(int)) == ESP_OK) {
       val = _noDelay;
@@ -120,6 +109,9 @@ bool WiFiServer::hasClient() {
     if (_accepted_sockfd >= 0) {
       return true;
     }
+    if (!_listening) {
+      return false;
+    }
     struct sockaddr_in _client;
     int cs = sizeof(struct sockaddr_in);
 #ifdef ESP_IDF_VERSION_MAJOR
@@ -134,6 +126,9 @@ bool WiFiServer::hasClient() {
 }
 
 void WiFiServer::end(){
+  if (_accepted_sockfd >= 0)
+    lwip_close(_accepted_sockfd);
+
 #ifdef ESP_IDF_VERSION_MAJOR
   lwip_close(sockfd);
 #else
